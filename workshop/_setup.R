f5a7a2295d5aa5e649a00fd64920c83c482d9d71
Alba Refoyo Martínez <44649699+albarema@users.noreply.github.com>
2026-05-01 11:51:11 +0200

    dev launch exercises v2

---
+++ workshop/_setup.R
@@ -0,0 +1,5 @@
+if (requireNamespace("webexercises", quietly = TRUE)) {
+  library(webexercises)
+  knitr::opts_chunk$set(echo = FALSE)
+  style_widgets(incorrect = "red", correct = "green", highlight = "firebrick")
+}
